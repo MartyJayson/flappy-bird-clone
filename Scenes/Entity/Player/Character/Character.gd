cf91a31073ab17263bc3026873a28b193df6f9da
dw <dw@dw>
2021-09-17 13:06:42 +0600

    Add Character and Score sounds

--- Scenes/Entity/Player/Character/Character.gd
+++ Scenes/Entity/Player/Character/Character.gd
@@ -5,6 +5,9 @@ signal died
 export(float) var flap_force: float = -400.0
 
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
+onready var flap_audio_player: AudioStreamPlayer2D = $FlapAudioPlayer
+onready var death_audio_player: AudioStreamPlayer2D = $DeathAudioPlayer
+onready var hit_audio_player: AudioStreamPlayer2D = $HitAudioPlayer
 
 var alive = true
 
@@ -15,11 +18,13 @@ func _input(event: InputEvent):
 func flap():
 	linear_velocity.y = flap_force
 	animated_sprite.play("flap")
+	flap_audio_player.play()
 
 func die():
 	if !alive: return
 	alive = false
 	animated_sprite.stop()
+	hit_audio_player.play()
 	print("Player Character Died")
 	emit_signal("died")
 
@@ -29,3 +34,6 @@ func _on_AnimatedSprite_animation_finished():
 func _on_Character_body_entered(body: Node):
 	if body.is_in_group("obstacles"):
 		die()
+
+func _on_HitAudioPlayer_finished():
+	death_audio_player.play()
--- Scenes/Environment/Obstacle/Pipe/Pipe.gd
+++ Scenes/Environment/Obstacle/Pipe/Pipe.gd
@@ -4,6 +4,8 @@ signal score
 
 export(float) var speed: float = -200.0
 
+onready var score_audio_player: AudioStreamPlayer2D = $ScoreAudioPlayer
+
 func _physics_process(delta):
 	position.x += speed * delta
 
@@ -13,4 +15,5 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_Area2D_body_entered(body: Node):
 	if body.is_in_group("players"):
 		emit_signal("score")
+		score_audio_player.play()
 
